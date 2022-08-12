2bc25f33b1068b179505c588f44733a14a5f239a
Nizar ILAHI <nizar.ilahi@gmail.com>
2022-08-12 18:25:32 +0100

    Adding the header to route properly our app, each link correspond certain route that means certain component

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 interface SignupCredentials {
   username: string,
@@ -16,6 +18,7 @@ interface SignupResponse {
 export class AuthService {
 
   endpointUrl: string = 'https://api.angular-email.com';
+  signedin$ = new BehaviorSubject(false);
 
   constructor(private http: HttpClient) { }
 
@@ -29,6 +32,10 @@ export class AuthService {
     return this.http.post<SignupResponse>(
       `${this.endpointUrl}/auth/signup`,
       credentials
+    ).pipe(
+      tap(() => {
+        this.signedin$.next(true);
+      })
     );
   }
 
